01c5c6d9a6dea0249ec7817f55679c3b0fad4079
Sam Lee <mail@samlee.xyz>
2023-11-25 14:03:15 -0500

    Reword User class documentation.

--- MiraeNet.Core/Discord/User.cs
+++ MiraeNet.Core/Discord/User.cs
@@ -2,48 +2,45 @@ using System.Text.Json.Serialization;
 
 namespace MiraeNet.Core.Discord;
 
-// TODO: Full spec coverage
-// TODO: GPT proofread
-
 /// <summary>
-///     Represents a Discord user object.
-///     For more information, see
+///     Represents a Discord user.
+///     For more information, see the
 ///     <a href="https://discord.com/developers/docs/resources/user">Discord Developer Documentation</a>.
 /// </summary>
 public class User
 {
     /// <summary>
-    ///     Gets or sets the user's ID.
+    ///     The unique identifier for the user.
     /// </summary>
     [JsonPropertyName("id")]
     public required string Id { get; set; }
 
     /// <summary>
-    ///     Gets or sets the user's username.
+    ///     The username of the user.
     /// </summary>
     [JsonPropertyName("username")]
     public required string Username { get; set; }
 
     /// <summary>
-    ///     Gets or sets the user's discriminator tag.
+    ///     The discriminator tag of the user.
     /// </summary>
     [JsonPropertyName("discriminator")]
     public required string Discriminator { get; set; }
 
     /// <summary>
-    ///     Gets or sets the user's display name, if set. For bots, this is the application name.
+    ///     The display name of the user, if set. For bots, this represents the application name.
     /// </summary>
     [JsonPropertyName("global_name")]
     public string? GlobalName { get; set; }
 
     /// <summary>
-    ///     Gets or sets the hash of the user's avatar.
+    ///     The hash of the user's avatar.
     /// </summary>
     [JsonPropertyName("avatar")]
     public string? Avatar { get; set; }
 
     /// <summary>
-    ///     Gets or sets a value indicating whether the user belongs to an OAuth2 application.
+    ///     A value indicating whether the user is associated with an OAuth2 application.
     /// </summary>
     [JsonPropertyName("bot")]
     public bool? IsBot { get; set; }
